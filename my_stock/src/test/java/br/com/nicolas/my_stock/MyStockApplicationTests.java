1c0a2587006c40c212675635e209f599809ffc72
nicolasdca <nicolasdavid193@gmail.com>
2024-04-05 08:41:54 -0300

    Configuraçoes iniciais

---
+++ my_stock/src/test/java/br/com/nicolas/my_stock/MyStockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nicolas.my_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
